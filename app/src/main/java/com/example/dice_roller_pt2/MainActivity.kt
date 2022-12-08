35fa583393909a58b2332d052fff97e2a0e98842
Muhammad Arham <67412607+Arham-12336@users.noreply.github.com>
2022-12-08 23:59:35 +0500

    Test toast message

--- app/src/main/java/com/example/dice_roller_pt2/MainActivity.kt
+++ app/src/main/java/com/example/dice_roller_pt2/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.dice_roller_pt2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button=findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this,"Button clicked",Toast.LENGTH_SHORT).show()
+        }
+
+
+
     }
 }
